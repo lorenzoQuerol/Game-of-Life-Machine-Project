e59625924d7f08f38ff3a67f0efb77d22aabf910
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2020-08-20 22:35:50 +0800

    First Commit

--- Decks.java
+++
@@ -1,3 +0,0 @@
-public class Decks {
-    private 
-}
--- Spinner.java
+++
@@ -1,7 +0,0 @@
- -import java.util.Random;
-
-public class Spinner {
-    Random rand = new Random();
-
-    int randNum = rand.nextInt(10 - 1 + 1) + 1; //rng 1-10
-}
