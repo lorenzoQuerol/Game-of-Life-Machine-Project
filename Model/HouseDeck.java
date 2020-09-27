77dc3825e0ee021084299576b5f1514f84cc9840
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2020-09-27 14:14:49 +0800

    Semi functional controllers
    winner scene needs to be changed, settle bank loan controllers are missing

--- Model/HouseDeck.java
+++ Model/HouseDeck.java
@@ -9,13 +9,5 @@ public class HouseDeck extends Deck<HouseCard> {
         for (int i = 0; i < 5; i++)
             temp.add(new HouseCard(HouseCard.HOUSE[i], HouseCard.HOUSE_ODD[i], HouseCard.HOUSE_EVEN[i]));
 
-        Collections.shuffle(temp);
-        Collections.shuffle(temp);
-
-        for (HouseCard c : temp)
-            deck.push(c);
-
-        temp.clear();
     }
-
 }
